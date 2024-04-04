d1b09f5e2f2b860a518836a48342fdfa70c786fa
0x1zen <rajdubalwork@gmail.com>
2024-04-04 20:52:36 +0530

    If else in php

---
+++ 04_ifelse.php
@@ -0,0 +1,22 @@
+<!-- if (7 > 3) {
+  echo "Hello There!";
+}
+
+
+$t = 77;
+
+if ($t == 14) {
+  echo "Have a good day!";
+} -->
+
+
+$a = 13;
+
+if ($a > 10) {
+  echo "Above 10";
+  if ($a > 20) {
+    echo " and also above 20";
+  } else {
+    echo " but not above 20";
+  }
+}
